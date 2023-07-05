87b67b253c323321bc774521c552346e207d08eb
Muhammad Bilal <bilaltheofficial@gmail.com>
2023-07-05 14:14:07 +0500

    create app component

--- app/javascript/controllers/react_controller.js
+++ app/javascript/controllers/react_controller.js
@@ -1,7 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
-
+import { Controller } from "@hotwired/stimulus";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../components/App";
 // Connects to data-controller="react"
 export default class extends Controller {
   connect() {
+    console.log("Connected!");
+    const app = document.getElementById("root");
+    const root = ReactDOM.createRoot(app);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
   }
 }
